a63ef8584816d9a457b672e60d9352bb49af0b99
¨isaias <isaiasrocha1626@gmail.com>
2024-06-04 20:55:00 -0300

    feat: add viewsets

--- order/models/order.py
+++ order/models/order.py
@@ -1,6 +1,7 @@
-from product.models.product import Product
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+
+from product.models import Product
 
 
 class Order(models.Model):
